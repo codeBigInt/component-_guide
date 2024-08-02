986d021dbf18a38ed1d1678174edaabbea8f1365
Lucky Elliot <elliotlucky99@gmail.com>
2024-08-02 03:43:18 +0100

    added_toast_component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,6 +26,11 @@ const config = {
         "navbar": "linear-gradient(181deg, #F7EEE7 100%, #F9E9A3 100%, #FD0 100%)"
       },
       keyframes: {
+        "scroll-btn": {
+          "0%": { transform: "translateY(0)"},
+          "50%": { transform: "translateY(3px)"},
+          "100%": { transform: "translateY(0)"}
+        },
         "accordion-down": {
           from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
@@ -36,6 +41,7 @@ const config = {
         },
       },
       animation: {
+        "scroll-btn": "scroll-btn 0.5s infinite",
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
